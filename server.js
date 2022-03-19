7c3ad202966e91ab1d660afce2cc4312b95a523c
tjswinson <tiffanyjswinson@gmail.com>
2022-03-19 13:09:55 -0400

    build simple server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/heartbeat',(req, res) => {
+    res.json({
+"is": "working"
+    })
+});
+
+server.listen('8080', () => {
+  console.log('The server is listening at port 8080');
+});
