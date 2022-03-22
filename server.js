87d4f161cf89b1a4cc086d3e1dda19816a7c210c
tjswinson <tiffanyjswinson@gmail.com>
2022-03-22 14:30:44 -0400

    add react-ui

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.static(path.resolve(__dirname + '/react-ui/build')));
+
 server.get('/heartbeat',(req, res) => {
     res.json({
 "is": "working"
     })
 });
+
+server.get('*', (req, res) => {
+  res.sendFile(path.resolve(_dirname + './react-ui/build/index.html'));
+})
 
 server.listen('8080', () => {
   console.log('The server is listening at port 8080');
